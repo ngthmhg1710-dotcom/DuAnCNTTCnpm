3b6613c975b0ba0b6f6755895b53d05e3d94d737
ngthmhg1710-dotcom <ngthmhg1710@gmail.com>
2026-09-12 12:03:43 +0700

    Fix background image path import for Login page

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -4,7 +4,7 @@ import { Button, Card, Typography, Tabs, Input, Checkbox, message, Alert } from 
 import { GoogleOutlined, UserOutlined, LockOutlined, CrownOutlined } from '@ant-design/icons'
 import { loginWithCredentials, loginWithOAuth } from '../../lib/api'
 
-const bgImage = '/img/background.jpg'
+import bgImage from '../../assets/background.jpg'
 
 export default function Login() {
   const [searchParams] = useSearchParams()
